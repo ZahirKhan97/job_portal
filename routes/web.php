8a62619b53e126e55fca49eea452eff2f8086465
ZahirKhan97 <GitHub: mzahirkhan302@gmail.com>
2024-07-12 01:38:26 +0500

    authentication completion

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+Route::group(['account'], function () {
+  // Guest Routes
+  Route::group(['middleware' => 'guest'], function () {
+    Route::get('/account/login', [AccountController::class, 'login'])->name('account.login');
+    Route::post('/account/authenticate', [AccountController::class, 'authenticate'])->name('account.authenticate');
+    Route::get('/account/register', [AccountController::class, 'registration'])->name('account.registration');
+    Route::post('/account/process-register', [AccountController::class, 'processRegistration'])->name('account.processRegister');
+  });
+
+  // Authenticated Routes
+  Route::group(['middleware' => 'auth'], function () {
+    Route::get('/account/profile', [AccountController::class, 'profile'])->name('account.profile');
+    Route::get('/account/logout', [AccountController::class, 'logout'])->name('account.logout');
+  });
+});
