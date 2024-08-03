d18c28d36fbd39406181c49a4f242d0e4508625d
ZahirKhan97 <GitHub: mzahirkhan302@gmail.com>
2024-08-03 13:02:11 +0500

    Feat: Searching on home Page Completed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,12 +11,14 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::where('status', 1)->orderBy('name', 'ASC')->take(8)->get();
+        $newCategories = Category::where('status', 1)->orderBy('name', 'ASC')->get();
         $featuredJobs = Job::where('status', 1)->orderBy('created_at', 'DESC')->with('jobType')->where('isFeatured', 1)->take(6)->get();
         $latestJobs = Job::where('status', 1)->orderBy('created_at', 'DESC')->with('jobType')->take(6)->get();
         return view('front.home', [
             'categories' => $categories,
             'featuredJobs' => $featuredJobs,
-            'latestJobs' => $latestJobs
+            'latestJobs' => $latestJobs,
+            'newCategories' => $newCategories
         ]);
     }
 }
