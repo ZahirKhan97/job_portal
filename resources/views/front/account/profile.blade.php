d3944efb124caf6c957918f2135eed3870b25c9a
ZahirKhan97 <GitHub: mzahirkhan302@gmail.com>
2024-08-04 15:08:23 +0500

    Feat: Update Password Completed

--- resources/views/front/account/profile.blade.php
+++ resources/views/front/account/profile.blade.php
@@ -48,24 +48,29 @@
               </div>
 
               <div class="card border-0 shadow mb-4">
-                  <div class="card-body p-4">
-                      <h3 class="fs-4 mb-1">Change Password</h3>
-                      <div class="mb-4">
-                          <label for="" class="mb-2">Old Password*</label>
-                          <input type="password" placeholder="Old Password" class="form-control">
-                      </div>
-                      <div class="mb-4">
-                          <label for="" class="mb-2">New Password*</label>
-                          <input type="password" placeholder="New Password" class="form-control">
-                      </div>
-                      <div class="mb-4">
-                          <label for="" class="mb-2">Confirm Password*</label>
-                          <input type="password" placeholder="Confirm Password" class="form-control">
-                      </div>                        
-                  </div>
-                  <div class="card-footer  p-4">
-                      <button type="button" class="btn btn-primary">Update</button>
-                  </div>
+                  <form action="" method="post" name="changePasswordForm" id="changePasswordForm">
+                    <div class="card-body p-4">
+                        <h3 class="fs-4 mb-1">Change Password</h3>
+                        <div class="mb-4">
+                            <label for="" class="mb-2">Old Password*</label>
+                            <input type="password" name="old_password" id="old_password" placeholder="Old Password" class="form-control">
+                            <p></p>
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="mb-2">New Password*</label>
+                            <input type="password" name="new_password" id="new_password" placeholder="New Password" class="form-control">
+                            <p></p>
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="mb-2">Confirm Password*</label>
+                            <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" class="form-control">
+                            <p></p>
+                        </div>                        
+                    </div>
+                    <div class="card-footer  p-4">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </div>
+                  </form>
               </div>                
           </div>
       </div>
@@ -103,6 +108,47 @@
                 }
                 else {
                     $('#email').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
+                }
+          }
+        }
+      });
+    });
+
+    $("#changePasswordForm").submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        url: '{{ route("account.updatePassword") }}',
+        type: 'post',
+        data: $("#changePasswordForm").serializeArray(),
+        dataType: 'json',
+        success: function(response){
+          if(response.status == true) 
+          {
+            $('#old_password').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
+            $('#new_password').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
+            $('#confirm_password').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
+            window.location.href="{{ route('account.profile') }}"
+          } 
+          else 
+          {
+            var errors = response.errors;
+            if(errors.old_password) {
+                    $('#old_password').addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(errors.old_password);
+                }
+                else {
+                    $('#old_password').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
+                }
+                if(errors.new_password) {
+                    $('#new_password').addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(errors.new_password);
+                }
+                else {
+                    $('#new_password').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
+                }
+                if(errors.confirm_password) {
+                    $('#confirm_password').addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(errors.confirm_password);
+                }
+                else {
+                    $('#confirm_password').removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
                 }
           }
         }
