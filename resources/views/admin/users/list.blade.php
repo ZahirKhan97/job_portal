936b99223b114b889fa4da617cc9e3e0564f47cc
ZahirKhan97 <GitHub: mzahirkhan302@gmail.com>
2024-08-07 16:05:32 +0500

    Mod: Users in Admin Panel Completed

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -55,8 +55,8 @@
                                               <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
                                           </button>
                                           <ul class="dropdown-menu dropdown-menu-end">
-                                              <li><a class="dropdown-item" href="{{ route('account.editJob',$user->id) }}"><i class="fa fa-edit" aria-hidden="true"></i> Edit</a></li>
-                                              <li><a class="dropdown-item" href="#" onclick="deleteJob({{ $user->id }})"><i class="fa fa-trash" aria-hidden="true"></i> Delete</a></li>
+                                              <li><a class="dropdown-item" href="{{ route('admin.users.edit',$user->id) }}"><i class="fa fa-edit" aria-hidden="true"></i> Edit</a></li>
+                                              <li><a class="dropdown-item" href="javascript:void(0)" onclick="deleteUser({{ $user->id }})"><i class="fa fa-trash" aria-hidden="true"></i> Delete</a></li>
                                           </ul>
                                       </div>
                                   </td>
@@ -78,4 +78,21 @@
 @endsection
 
 @section('customJs')
+<script type="text/javascript">
+    function deleteUser(id)
+    {
+        if(confirm("Are you sure you want to delete ?"))
+        {
+         $.ajax({
+            type: "delete",
+            url: "{{ route('admin.users.destroy') }}",
+            data: {id:id},
+            dataType: "json",
+            success: function (response) {
+                window.location.href = "{{ route('admin.users') }}"
+            }
+         });   
+        }
+    }
+</script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::post('save-job', [JobsController::class, 'saveJob'])->name('saveJob');
 Route::group(['prefix' => 'admin', 'middleware' => 'checkRole'], function () {
   Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
   Route::get('/users', [UserController::class, 'index'])->name('admin.users');
+  Route::get('/users/{id}', [UserController::class, 'edit'])->name('admin.users.edit');
+  Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
+  Route::delete('/users', [UserController::class, 'destroy'])->name('admin.users.destroy');
 });
 
 Route::group(['prefix' => 'account'], function () {
